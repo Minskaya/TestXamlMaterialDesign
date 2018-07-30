0ab77b88d6fa437f1305f646b12afca45d6ef73a
Minskaya <Minskaya@XPS.(none)>
2018-07-30 17:09:08 +0200

    1ere version

---
+++ TestXamlMaterialDesign/TestXamlMaterialDesign/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestXamlMaterialDesign
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // force load material desgn dll
+            var card = new MaterialDesignThemes.Wpf.Card();
+            var hue = new MaterialDesignColors.Hue(
+                "Dummy",
+                System.Windows.Media.Colors.Black,
+                System.Windows.Media.Colors.White);
+        }
+    }
+}
